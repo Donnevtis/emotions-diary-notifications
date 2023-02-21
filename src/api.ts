38a04efa5c31067922359ad01a7f7fad04a7dbbd
Donnevtis <nevtees@gmail.com>
2023-02-21 23:52:12 +0300

    Squashed commit of the following:
    commit 1f8e288f762b3f99a654daf24154d7aee2f0c396
    Author: Donnevtis <nevtees@gmail.com>
    Date:   Tue Feb 21 23:51:17 2023 +0300
    
        get language from user metadata
    
    commit d99606e89b8fb425bae6fc665254fc613b956b22
    Author: Donnevtis <nevtees@gmail.com>
    Date:   Sun Feb 12 19:35:15 2023 +0300
    
        bump to alpha 0.0.1

--- src/api.ts
+++ src/api.ts
@@ -2,31 +2,76 @@ import bot from './bot';
 import { UserTimersSettings } from './types';
 import { i18n } from './i18n';
 import { getToken } from './auth';
-import fetch from 'node-fetch';
+import { errorHandler } from './utils';
+import { GetItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import dynamodb from './client';
 
-export const findUsersByTimer = (timer: string) => {
-  const url = new URL(String(process.env.DB_URL));
-  url.searchParams.append('time', timer);
+const dbErrorHandler = errorHandler('Database exception');
 
-  return fetch(url, {
-    method: 'GET',
-  }).then(async res => {
-    if (res.status !== 200) return null;
-    return (await res.json()) as UserTimersSettings[];
-  });
-};
-
-export const sendNotification = ({ notify, language_code, user_id }: UserTimersSettings) => {
+export const sendNotification = ({
+  notify,
+  user_id,
+  language_code,
+}: Pick<UserTimersSettings, 'notify' | 'user_id' | 'language_code'>) => {
   if (!notify) return;
 
   const token = getToken({ id: user_id });
 
   const url = new URL(String(process.env.WEB_APP_URL));
-  url.searchParams.append('token', token);
+  url.searchParams.set('token', token);
+  url.searchParams.set('lang', language_code);
 
   const t = i18n(language_code);
 
   bot.sendMessage(user_id, t('hawaryou')!, {
     reply_markup: { inline_keyboard: [[{ text: t('diary'), web_app: { url: url.toString() } }]] },
   });
+};
+
+export const getUserLanguage = async (id: number): Promise<string | null> => {
+  try {
+    const input = new GetItemCommand({
+      TableName: 'Users',
+      Key: marshall({
+        PK: `user#${id}`,
+        SK: `#metadata#${id}`,
+      }),
+      ProjectionExpression: 'language_code',
+    });
+
+    const { Item } = await dynamodb.send(input);
+
+    return Item ? unmarshall(Item).language_code : null;
+  } catch (error) {
+    return dbErrorHandler(error, getUserLanguage.name, {
+      id,
+    });
+  }
+};
+
+export const findUsersByTimer = async (time: string) => {
+  try {
+    const input = new QueryCommand({
+      TableName: 'Users',
+      IndexName: 'InvertedIndex',
+      KeyConditionExpression: 'SK = :sk',
+      FilterExpression: 'contains(reminder_timers, :t)',
+      ExpressionAttributeValues: marshall({
+        ':sk': 'reminders',
+        ':t': time,
+      }),
+      Select: 'SPECIFIC_ATTRIBUTES',
+      ProjectionExpression: 'user_id, notify, time_offset',
+    });
+
+    const { Items } = await dynamodb.send(input);
+
+    return Items?.length ? (Items.map(user => unmarshall(user)) as UserTimersSettings[]) : [];
+  } catch (error) {
+    dbErrorHandler(error, findUsersByTimer.name, {
+      time,
+    });
+    return [];
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Handler } from '@yandex-cloud/function-types';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { findUsersByTimer, sendNotification } from './api';
+import { findUsersByTimer, getUserLanguage, sendNotification } from './api';
+import { errorHandler, logger } from './utils';
 
 dayjs.extend(customParseFormat);
 
 const TIME_FORMAT = 'HH:mm Z';
+
+const functionErrorHandler = errorHandler('Function error.');
 
 export const handler: Handler.Timer = async ({
   messages: [
@@ -16,8 +19,24 @@ export const handler: Handler.Timer = async ({
 }) => {
   const timer = dayjs(created_at).format(TIME_FORMAT);
 
-  findUsersByTimer(timer).then(usersSettings => {
-    if (!usersSettings) return;
-    usersSettings.forEach(sendNotification);
-  });
+  findUsersByTimer(timer)
+    .then(users => {
+      const countUsers = users.length;
+
+      logger.info(`Search for a notification at ${timer}. ${countUsers} users found.`);
+
+      if (!countUsers) return;
+
+      users.forEach(async ({ user_id, notify, language_code }) =>
+        sendNotification({
+          user_id,
+          notify,
+          language_code: (await getUserLanguage(user_id)) || language_code,
+        }),
+      );
+    })
+    .catch(error => {
+      functionErrorHandler(error, handler.name, { timer });
+      throw new Error('Notification error!');
+    });
 };
